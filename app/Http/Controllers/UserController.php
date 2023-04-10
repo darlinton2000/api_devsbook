1f48c528a15952d3bc673009c048e6cbb35c1592
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-04-09 22:37:26 -0300

    Método para atualizar os dados do usuário criado

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,5 +13,78 @@ class UserController extends Controller
     {
         $this->middleware('auth:api');
         $this->loggedUser = auth()->user();
+    }
+
+    /**
+     * Faz a atualizacao dos dados do usuario
+     * @param Request $request
+     * @return string[]
+     */
+    public function update(Request $request)
+    {
+        $array = ['error' => ''];
+
+        // Recebendo os dados
+        $name             = $request->input('name');
+        $email            = $request->input('email');
+        $birthdate        = $request->input('birthdate');
+        $city             = $request->input('city');
+        $work             = $request->input('work');
+        $password         = $request->input('password');
+        $password_confirm = $request->input('password_confirm');
+
+        $user = User::find($this->loggedUser['id']);
+
+        // Name
+        if ($name) {
+            $user->name = $name;
+        }
+
+        // E-mail
+        if ($email) {
+            if ($email != $user->email) {
+                $emailExist = User::where('email', $email)->count();
+                if ($emailExist === 0) {
+                    $user->email = $email;
+                } else {
+                    $array['error'] = 'E-mail ja existe!';
+                    return $array;
+                }
+            }
+        }
+
+        // Birthdate
+        if ($birthdate) {
+            if (strtotime($birthdate) === false) {
+                $array['error'] = 'Data de nascimento invalida!';
+                return $array;
+            }
+            $user->birthdate = $birthdate;
+        }
+
+        // City
+        if ($city) {
+            $user->city = $city;
+        }
+
+        // Work
+        if ($work) {
+            $user->work = $work;
+        }
+
+        // Password
+        if ($password && $password_confirm) {
+            if ($password === $password_confirm) {
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+                $user->password = $hash;
+            } else {
+                $array['error'] = 'As senhas nao batem.';
+                return $array;
+            }
+        }
+
+        $user->save();
+
+        return $array;
     }
 }
