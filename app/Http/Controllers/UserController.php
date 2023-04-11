8e444dc1bdcfef6a29550eee4d301d293287b50e
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-04-10 22:09:30 -0300

    Criado a função upload do avatar do usuário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Intervention\Image\Facades\Image;
 
 class UserController extends Controller
 {
     private $loggedUser;
+
     public function __construct()
     {
         $this->middleware('auth:api');
@@ -84,6 +86,46 @@ class UserController extends Controller
         }
 
         $user->save();
+
+        return $array;
+    }
+
+    /**
+     * Faz a atualizacao do avatar do usuario
+     * @param Request $request
+     * @return string[]
+     */
+    public function updateAvatar(Request $request)
+    {
+        $array = ['error' => ''];
+        $allowedTypes = ['image/jpg', 'image/jpeg', 'image/png'];
+
+        $image = $request->file('avatar');
+
+        if ($image) {
+            if (in_array($image->getClientMimeType(), $allowedTypes)) {
+
+                $filename = md5(time() . rand(0,9999)) . '.jpg';
+
+                $destPath = public_path('/media/avatars');
+
+                $img = Image::make($image->path())
+                        ->fit(200,200)
+                        ->save($destPath . '/' . $filename);
+
+                $user = User::find($this->loggedUser['id']);
+                $user->avatar = $filename;
+                $user->save();
+
+                $array['url'] = url('/media/avatars/' . $filename);
+            } else {
+                $array['error'] = 'Arquivo nao suportado!';
+                return $array;
+            }
+        } else {
+            $array['error'] = 'Arquivo nao enviado!';
+            return $array;
+        }
 
         return $array;
     }
