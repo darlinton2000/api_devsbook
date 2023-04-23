28e070326951ca2675d808b3a0573fff20e0149a
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-04-23 11:35:03 -0300

    Adicionando endpoints

--- routes/api.php
+++ routes/api.php
@@ -33,6 +33,9 @@ Route::post('/user/cover', 'UserController@updateCover');
 Route::get('/feed', 'FeedController@read');
 Route::get('/user/feed', 'FeedController@userFeed');
 Route::get('/user/{id}/feed', 'FeedController@userFeed');
+//Route::post('/user/{id}/follow', 'UserController@follow');
+//Route::get('/user/{id}/followers', 'UserController@followers');
+//Route::get('/user/{id}/photos', 'UserController@photos');
 
 Route::get('/user', 'UserController@read');
 Route::get('/user/{id}', 'UserController@read');
