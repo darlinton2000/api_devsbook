b60d5a24bd878dfea239fcd21826d7a266a1422b
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-04-23 10:36:21 -0300

    Função dar like no post implementada

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+use App\PostLike;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,5 +14,39 @@ class PostController extends Controller
     {
         $this->middleware('auth:api');
         $this->loggedUser = auth()->user();
+    }
+
+    public function like($id)
+    {
+        $array = ['error' => ''];
+
+        // Verificando se o post existe
+        $postExists = Post::find($id);
+
+        if ($postExists) {
+            $isLiked = PostLike::where('id_post', $id)->where('id_user', $this->loggedUser['id'])->count();
+
+            if ($isLiked > 0) {
+                $pl = PostLike::where('id_post', $id)->where('id_user', $this->loggedUser['id'])->first();
+                $pl->delete();
+
+                $array['isLiked'] = false;
+            } else {
+                $newPostLike = new PostLike();
+                $newPostLike->id_post = $id;
+                $newPostLike->id_user = $this->loggedUser['id'];
+                $newPostLike->created_at = date('Y-m-d H:i:s');
+                $newPostLike->save();
+
+                $array['isLiked'] = true;
+            }
+
+            $array['likeCount'] = PostLike::where('id_post', $id)->count();
+        } else {
+            $array['error'] = 'Post nao existe!';
+            return $array;
+        }
+
+        return $array;
     }
 }
